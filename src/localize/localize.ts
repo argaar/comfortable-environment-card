6cd8dcb72cc40b3ecb32baafba1819e200829609
Philippe Gressé-Lugué <GaPhi@users.noreply.github.com>
2023-12-18 14:32:45 +0100

    FR translation

--- src/localize/localize.ts
+++ src/localize/localize.ts
@@ -1,9 +1,11 @@
 import * as en from "./languages/en.json";
+import * as fr from "./languages/fr.json";
 import * as it from "./languages/it.json";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const languages: any = {
   en: en,
+  fr: fr,
   it: it,
 };
 
